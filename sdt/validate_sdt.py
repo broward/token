3e9d5624dda07cbba4d6f2e315b2a3ae259cd53c
Broward Horne <broward@startmail.com>
2024-12-02 11:26:29 -0500

    schema in a working state

--- sdt/validate_sdt.py
+++ sdt/validate_sdt.py
@@ -9,13 +9,15 @@ schema_dir = pathlib.Path(__file__).parent / './schema'
 data_dir = pathlib.Path(__file__).parent / './test'
 
 catalog.add_uri_source(URI('https://example.com/'), LocalSource(schema_dir, suffix='.json'))
+sdt_schema = catalog.get_schema(URI('https://example.com/sdt-schema'))
 
-# catalog.add_schema(URI('https://example.com/header'), 'header')
-# sdt_schema = catalog.get_schema(URI('https://example.com/sdt'))
-org_data = JSON.loadf(data_dir / 'header.json')
-org_schema = catalog.get_schema(URI('https://example.com/sdt-schema'))
+header = JSON.loadf(data_dir / 'header.json')
+person = JSON.loadf(data_dir / 'person.json')
+address = JSON.loadf(data_dir / 'address.json')
+status = JSON.loadf(data_dir / 'status.json')
 
-print(org_data)
-result = org_schema.evaluate(org_data)
-print(result.__dict__)
-print(result.output('flag'))
+print("header test: " + str(sdt_schema.evaluate(header)))
+print("person test: " + str(sdt_schema.evaluate(person)))
+print("address test: " + str(sdt_schema.evaluate(address)))
+print("status test: " + str(sdt_schema.evaluate(status)))
+
