6a40aeb0dbcffaa1fe0e878317258a5d918a6b67
Broward Horne <browardhorne@gmail.com>
2024-12-09 21:52:40 -0500

    save

--- scripts/python_setup.sh
+++ scripts/python_setup.sh
@@ -20,12 +20,17 @@ python3.9 get-pip.py --user
 # server
 pip install requests
 pip install dynaconf
+pip install fastapi
+pip install nacl.signing
+pip install boto3
 
 # database
 pip install flask
 pip install jsonify
 pip install peewee
 pip install psycopg2-binary
+
+
 
 # https://python-jsonschema.readthedocs.io/en/stable/
 pip install --upgrade jsonschema
