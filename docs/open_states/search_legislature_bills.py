a3ee7c66d4bdcc545a51158cb952e8d60c2f6ff7
Broward Horne <browardhorne@gmail.com>
2025-02-28 20:33:01 -0500

    20 more mailings

--- docs/open_states/search_legislature_bills.py
+++ docs/open_states/search_legislature_bills.py
@@ -41,7 +41,7 @@ def search_bills_by_keyword(page, keyword, jurisdiction="all", session=None):
         "page": page,
         "per_page": 20,
         # "jurisdiction": jurisdiction,
-        "created_since": "2025-02-25",
+        "created_since": "2025-02-27",
         "include": ["sponsorships"],
         "apikey": API_KEY,
         "session": session
