96e71815f84073d58421371506d2ac54edd4cd87
Broward Horne <browardhorne@gmail.com>
2025-05-08 09:38:55 -0400

    save

--- docs/open_states/search_legislature_bills.py
+++ docs/open_states/search_legislature_bills.py
@@ -41,7 +41,7 @@ def search_bills_by_keyword(page, keyword, jurisdiction="all", session=None):
         "page": page,
         "per_page": 20,
         # "jurisdiction": jurisdiction,
-        "updated_since": "2025-05-02",
+        "updated_since": "2025-05-07",
         "include": ["sponsorships"],
         "apikey": API_KEY,
         "session": session
@@ -134,13 +134,13 @@ def main():
     session = "" # input("Enter legislative session (leave blank for all sessions): ").strip() or None
     
     size = 1
-    keyword = "cryptocurrency"
+    #keyword = "cryptocurrency"
     #keyword = "bitcoin"
     #keyword = "bullion depository"
     #keyword = "gold currency"
-    #keyword = "blockchain"
+    keyword = "blockchain"
     #keyword = "stablecoin"
-    #keyword = "metals depository"
+    #keyword = "precious metals digital assets"
     #keyword = "crypto currency"
     #keyword = "bullion and currency"
 
